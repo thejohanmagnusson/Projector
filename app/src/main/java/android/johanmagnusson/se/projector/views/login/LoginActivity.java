7da5ac130be42c685a9fa3bdc09bd0cf236cd41e
Johan Magnusson <github@johanmagnusson.se>
2016-07-11 17:52:37 +0200

    Fix: Finalise sign in. Fix: Move OAuth client id from google-services.json to gradle.properties, bug in gradle so value wont be available in string resources. Fix: Downgrade Firebase to  9.2.0, no Google play services with correct version available in the latest emulator images.

--- app/src/main/java/android/johanmagnusson/se/projector/views/login/LoginActivity.java
+++ app/src/main/java/android/johanmagnusson/se/projector/views/login/LoginActivity.java
@@ -1,11 +1,16 @@
 package android.johanmagnusson.se.projector.views.login;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.johanmagnusson.se.projector.BuildConfig;
 import android.johanmagnusson.se.projector.MainActivity;
 import android.johanmagnusson.se.projector.R;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +21,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -28,16 +34,26 @@ import com.google.firebase.auth.GoogleAuthProvider;
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = LoginActivity.class.getSimpleName();
+
+    private static final String KEY_RESOLVING_ERROR = "resolving_error";
+
     private static final int RC_SIGN_IN = 9001;
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    private static final String DIALOG_ERROR = "dialog_error";
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mFirebaseAuthListener;
 
     private GoogleApiClient mGoogleApiClient;
+    private boolean mResolvingError = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(savedInstanceState != null) {
+            mResolvingError = savedInstanceState.getBoolean(KEY_RESOLVING_ERROR, false);
+        }
 
         setContentView(R.layout.activity_google);
 
@@ -49,7 +65,10 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         });
 
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("CHANGE THIS")
+                // BUG: Gradle bug, resource string is not always created.
+                //.requestIdToken(getString(R.string.default_web_client_id))
+                // Workaround: Place Firebase OAuth client_id in global gradle properties instead.
+                .requestIdToken(BuildConfig.FIREBASE_OAUTH_CLINET_ID)
                 .requestEmail()
                 .build();
 
@@ -67,13 +86,12 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 FirebaseUser user = firebaseAuth.getCurrentUser();
 
                 if(user != null) {
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Log.d(TAG, "---- onAuthStateChanged:signed_in:" + user.getUid());
+                    onAuthenticated();
                 }
                 else {
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Log.d(TAG, "---- onAuthStateChanged:signed_out");
                 }
-
-                onAuthenticated();
             }
         };
     }
@@ -95,6 +113,12 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             mFirebaseAuth.removeAuthStateListener(mFirebaseAuthListener);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(KEY_RESOLVING_ERROR, mResolvingError);
+        super.onSaveInstanceState(outState);
+    }
+
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
@@ -113,8 +137,17 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 firebaseAuthWithGoogle(account);
             }
             else {
-                // Sing in failed
+                // Sign in failed
                 // TODO: show error to user
+            }
+        }
+        else if(requestCode == REQUEST_RESOLVE_ERROR) {
+            mResolvingError = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if(!mGoogleApiClient.isConnecting() && !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
             }
         }
     }
@@ -128,14 +161,14 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+                        Log.d(TAG, "---- signInWithCredential:onComplete:" + task.isSuccessful());
 
                         // Successful login is notified to the mFirebaseAuthListener.
 
                         // If sign in fails, display a message to the user.
                         if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithCredential", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            Log.w(TAG, "---- signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "---- Authentication failed.", Toast.LENGTH_LONG).show();
                         }
 
                         // TODO: hide progress?
@@ -153,7 +186,60 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     //OnConnectionFailedListener.onConnectionFailed
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d(TAG, "onConnectionFailed:" + connectionResult);
-        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+        if(mResolvingError) {
+            // Already trying to resolve the error.
+            return;
+        }
+        else if(connectionResult.hasResolution()) {
+            try {
+                // Try to resolve the error.
+                mResolvingError = true;
+                connectionResult.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            }
+            catch (IntentSender.SendIntentException e) {
+                // Try again.
+                mGoogleApiClient.connect();
+            }
+        }
+        else {
+            showErrorDialog(connectionResult.getErrorCode());
+            mResolvingError = true;
+        }
+    }
+
+    /* Creates a dialog for an error message */
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getSupportFragmentManager(), ErrorDialogFragment.TAG);
+    }
+
+    /* Called from ErrorDialogFragment when the dialog is dismissed. */
+    public void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    /* A fragment to display an error dialog */
+    public static class ErrorDialogFragment extends DialogFragment {
+        public static final String TAG = ErrorDialogFragment.class.getSimpleName();
+
+        public ErrorDialogFragment() { }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    this.getActivity(), errorCode, REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((LoginActivity) getActivity()).onDialogDismissed();
+        }
     }
 }
