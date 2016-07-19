fbd8d95fd6c5e10ec052e53069b39ebbd2cbf7f3
Johan Magnusson <github@johanmagnusson.se>
2016-07-19 13:42:21 +0200

    New: Add contract details to project model

--- app/src/main/java/android/johanmagnusson/se/projector/model/Project.java
+++ app/src/main/java/android/johanmagnusson/se/projector/model/Project.java
@@ -18,13 +18,55 @@ public class Project {
         return number;
     }
 
+    private int price;
+    public int getPrice() { return price; }
+
+    private int costInternalMaterial;
+    public int getCostInternalMaterial() { return costInternalMaterial; }
+
+    private int costExternalMaterial;
+    public int getCostExternalMaterial() { return costExternalMaterial; }
+
+    private int costOutsourced;
+    public int getCostOutsourced() { return costOutsourced; }
+
+    private int costOther;
+    public int getCostOther() { return costOther; }
+
+    private int hoursManager;
+    public int getHoursManager() { return hoursManager; }
+
+    private int hoursDesigner;
+    public int getHoursDesigner() { return hoursDesigner; }
+
+    private int hoursWorker;
+    public int getHoursWorker() { return hoursWorker; }
+
     public Project(){
         // Default constructor required for calls to DataSnapshot.getValue(Project.class)
     }
 
-    public Project(String name, String number){
+    public Project(String name,
+                   String number,
+                   int price,
+                   int costInternalMaterial,
+                   int costExternalMaterial,
+                   int costOutsourced,
+                   int costOther,
+                   int hoursManager,
+                   int hoursDesigner,
+                   int hoursWorker){
+
         this.name = name;
         this.number = number;
+        this.price = price;
+        this.costInternalMaterial = costInternalMaterial;
+        this.costExternalMaterial = costExternalMaterial;
+        this.costOutsourced = costOutsourced;
+        this.costOther = costOther;
+        this.hoursManager = hoursManager;
+        this.hoursDesigner = hoursDesigner;
+        this.hoursWorker = hoursWorker;
     }
 
     @Exclude
@@ -33,6 +75,14 @@ public class Project {
 
         result.put("name", name);
         result.put("number", number);
+        result.put("price", price);
+        result.put("costInternalMaterial", costInternalMaterial);
+        result.put("costExternalMaterial", costExternalMaterial);
+        result.put("costOutsourced", costOutsourced);
+        result.put("costOther", costOther);
+        result.put("hoursManager", hoursManager);
+        result.put("hoursDesigner", hoursDesigner);
+        result.put("hoursWorker", hoursWorker);
 
         return result;
     }
